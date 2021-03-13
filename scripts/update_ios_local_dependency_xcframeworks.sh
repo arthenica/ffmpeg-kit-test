b7e0ec75fecca271d425817750b148c184e1131b
Taner Sener <tanersener@gmail.com>
2021-03-13 09:31:54 +0000

    update ios xcframework copy scripts

--- scripts/update_ios_local_dependency_xcframeworks.sh
+++ scripts/update_ios_local_dependency_xcframeworks.sh
@@ -3,5 +3,6 @@
 export BASEDIR="$(pwd)"
 
 cd ${BASEDIR}/../ios/test-app-local-dependency || exit 1
+rm -rf *.framework || exit 1
 rm -rf *.xcframework || exit 1
 find ${BASEDIR}/../../ffmpeg-kit/prebuilt/bundle-apple-xcframework-ios -name "*.xcframework" -exec cp -r {} . \; || exit 1
